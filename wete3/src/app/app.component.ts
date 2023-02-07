160facba797e12f028fe519bbf54a00fc1e2153f
Eldam804 <adam.kleinedler@gmail.com>
2023-02-07 18:32:09 +0100

    Functional version of school project

--- wete3/src/app/app.component.ts
+++ wete3/src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import {FormControl, FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  templateUrl:
+    './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'wete3';
+  title = 'Názov projektu';
+  form: FormGroup;
+  constructor() {
+    this.form = new FormGroup({
+      projectTitle: new FormControl()
+    })
+  }
+  changeProjectTitle(title: string): void {
+    this.title = title;
+  }
+
 }
