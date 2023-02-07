f0977fbe619a9bfcfde454c4ea2481d954c1402d
Eldam804 <adam.kleinedler@gmail.com>
2023-02-07 20:57:22 +0100

    Pokus

--- wete3/src/app/app.component.ts
+++ wete3/src/app/app.component.ts
@@ -9,14 +9,22 @@ import {FormControl, FormGroup} from "@angular/forms";
 })
 export class AppComponent {
   title = 'Názov projektu';
+  check;
   form: FormGroup;
   constructor() {
     this.form = new FormGroup({
-      projectTitle: new FormControl()
-    })
+      projectTitle: new FormControl(),
+    }),
+    this.check = new Array(8);
+    for (let i = 0; i < this.check.length; i++) {
+      this.check.push({checked: false});
+    }
   }
   changeProjectTitle(title: string): void {
     this.title = title;
   }
-
+  onChangeCheck(n :number): void{
+    this.check[n].checked = !this.check[n].checked;
+    console.debug(this.check);
+  }
 }
