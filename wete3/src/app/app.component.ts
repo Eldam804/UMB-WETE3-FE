7895f6f79055a7f680bdccab626a0c73cb85d985
Eldam804 <adam.kleinedler@gmail.com>
2023-02-07 21:34:39 +0100

    Final version, added two functions that can convert bin to dec && dec to bin

--- wete3/src/app/app.component.ts
+++ wete3/src/app/app.component.ts
@@ -8,23 +8,37 @@ import {FormControl, FormGroup} from "@angular/forms";
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Názov projektu';
-  check;
-  form: FormGroup;
-  constructor() {
-    this.form = new FormGroup({
-      projectTitle: new FormControl(),
-    }),
-    this.check = new Array(8);
-    for (let i = 0; i < this.check.length; i++) {
-      this.check.push({checked: false});
+  enteredNumber = 0;
+  check2 = [false, false, false, false, false, false, false, false];
+  onChangeCheck(n :number): void{
+    this.check2[n] = !this.check2[n];
+    let binary = "";
+    for (let i = 0; i < this.check2.length; i++) {
+      if(this.check2[i]){
+        binary += "1"
+      }else{
+        binary += "0"
+      }
+    }
+    this.enteredNumber = parseInt(binary, 2);
+
+    console.debug(this.check2);
+  }
+  onChangeNumber(n: number): void{
+    if(n <= 255 && n >= 0){
+      let binary = n.toString(2);
+      while(binary.length < 8){
+        binary = "0" + binary;
+      }
+      for (let i = 0; i < binary.length; i++) {
+        if(binary[i] == "0"){
+          this.check2[i] = false;
+        }else{
+        this.check2[i] = true;
+        }
+      }
+      console.debug(binary)
     }
   }
-  changeProjectTitle(title: string): void {
-    this.title = title;
-  }
-  onChangeCheck(n :number): void{
-    this.check[n].checked = !this.check[n].checked;
-    console.debug(this.check);
-  }
+
 }
