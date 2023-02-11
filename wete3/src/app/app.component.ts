7189dfa6124eacaefd22298258ceb928f6f0d2a8
tomas <jokaitomas@gmail.com>
2023-02-11 23:07:46 +0100

    Test

--- wete3/src/app/app.component.ts
+++ wete3/src/app/app.component.ts
@@ -7,16 +7,37 @@ import {FormControl, FormGroup} from "@angular/forms";
     './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Názov projektu';
-  form: FormGroup;
-  constructor() {
-    this.form = new FormGroup({
-      projectTitle: new FormControl()
-    })
-  }
-  changeProjectTitle(title: string): void {
-    this.title = title;
-  }
 
+
+export class AppComponent {
+  enteredNumber=0;
+  checkbox = [false,false,false,false,false,false,false,false];
+  ZmenaCheckboxu(cislo :number): void{
+
+    this.checkbox[cislo] = !this.checkbox[cislo];
+    let binary = "";
+    for (let i = 0; i<8;i++) {
+      if (this.checkbox) {
+        binary += "1"
+      } else {
+        binary += "0"
+      }
+    }
+      this.enteredNumber = parseInt(binary,2);
+  }
+  ZmenaCislo (cislo :number): void{
+    if(cislo<=255 && cislo >=0) {
+      let binary = cislo.toString(2);
+      while (binary.length < 8) {
+        binary = "0" + binary;
+      }
+      for (let i = 0; i < binary.length; i++) {
+        if (binary[i] == "0") {
+          this.checkbox[i] = false;
+        } else {
+          this.checkbox[i] = true;
+        }
+      }
+    }
+  }
 }
