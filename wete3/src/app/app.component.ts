ec434fc7d702f05e0442919ad33634339ab0ca35
Eldam804 <adam.kleinedler@gmail.com>
2023-02-18 20:00:59 +0100

    Added 3 forms for borrowings, books and users

--- wete3/src/app/app.component.ts
+++ wete3/src/app/app.component.ts
@@ -1,21 +1,73 @@
 import { Component } from '@angular/core';
 import {FormControl, FormGroup} from "@angular/forms";
 
+
+export enum Menu{
+  BOOKS = "BOOKS",
+  USERS = "USERS",
+  BORROWINGS = "BORROWINGS"
+}
+
 @Component({
   selector: 'app-root',
   templateUrl:
     './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   form: FormGroup;
+  formBooks: FormGroup;
+  formBorrowings: FormGroup;
+  menu = Menu;
+  actualMenu: Menu = Menu.USERS
+  persons: Array<{
+    id: number;
+    name: string;
+    surname: string
+  }> = [];
+  books: Array<{
+    id: number;
+    title: string;
+    author: string;
+    available: number
+  }> = [];
+  borrowed: Array<{
+    id: number;
+    user: string;
+    book: string;
+  }> = [];
   constructor() {
     this.form = new FormGroup({
+      id: new FormControl(),
       name: new FormControl(),
       surname: new FormControl()
     });
+    this.formBooks = new FormGroup({
+      id: new FormControl(),
+      title: new FormControl(),
+      author: new FormControl(),
+      available: new FormControl()
+    });
+    this.formBorrowings = new FormGroup({
+      id: new FormControl(),
+      user: new FormControl(),
+      book: new FormControl()
+    });
   }
   savePerson(): void {
-    console.log("Osoba:", this.form.value);
+    this.persons.push(this.form.value);
+    this.form.reset();
+  }
+  addBook(): void{
+    this.books.push(this.formBooks.value);
+    this.formBooks.reset();
+  }
+  addBorrowed(): void {
+    this.borrowed.push(this.formBorrowings.value);
+    this.formBorrowings.reset();
+  }
+  changeMenu(changedMenu: Menu): void {
+    this.actualMenu = changedMenu;
   }
 }
