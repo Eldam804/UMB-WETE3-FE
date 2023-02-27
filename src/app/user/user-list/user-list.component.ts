ad15257b03cadbd0d8c7befa300fbd3bcae72792
Eldam804 <adam.kleinedler@gmail.com>
2023-02-27 19:35:05 +0100

    Non functional version of school project

---
+++ src/app/user/user-list/user-list.component.ts
@@ -0,0 +1,25 @@
+import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {User} from "../../model/user.model";
+
+@Component({
+  selector: 'app-user-list',
+  templateUrl: './user-list.component.html',
+  styleUrls: ['./user-list.component.css']
+})
+export class UserListComponent {
+  @Input()
+  persons: Array<User> = [];
+
+  @Output()
+  personToUpdate = new EventEmitter<number>();
+
+  @Output()
+  personToDelete = new EventEmitter<number>();
+  deletePerson(index: number): void {
+    this.personToDelete.emit(index);
+  }
+
+  editPerson(userId: number): void {
+    this.personToUpdate.emit(userId);
+  }
+}
