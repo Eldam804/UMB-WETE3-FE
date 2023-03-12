5d5450234790b96e772adbc323ec0536002f73e2
Tomas306 <124598342+Tomas306@users.noreply.github.com>
2023-03-12 20:36:41 +0100

    add get Borrow

---
+++ src/app/common/service/borrowings.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import {Observable} from "rxjs";
+import {Borrow} from "../../model/borrow.model";
+import {HttpClient} from "@angular/common/http";
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BorrowingsService {
+  private url = 'http://localhost:8080/api/customer';
+  constructor(private http: HttpClient) {}
+
+    getBorrows(): Observable<Borrow[]>{
+      return this.http.get<Borrow[]>(this.url);
+    }
+    getBorrow(borrowId: number): Observable<Borrow>{
+      return this.http.get<Borrow>(this.url + "/" + borrowId);
+    }
+    createBorrow(borrow: Borrow): Observable<number>{
+      return this.http.post<number>(this.url, borrow);
+    }
+
+  updateBorrow(borrow: Borrow): Observable<number>{
+      return this.http.put<number>(this.url + "/" + borrow.id, borrow);
+    }
+    deleteBorrow(borrowId: number): Observable<void>{
+      return this.http.delete<void>(this.url + "/" + borrowId);
+    }
+
+
+
+}
